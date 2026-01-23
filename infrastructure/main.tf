2ff32782caf9a330d525af157f3aef0fea4aacca
VaibhavKumar2005 <vaibhavkrsharma0412@gmail.com>
2026-01-23 15:03:53 +0530

    docs: unified gitignore for all microservices and infra

---
+++ infrastructure/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-azure-rag-dev"
+  location = "East US"
+}
+
+# 2. Container Registry
+resource "azurerm_container_registry" "acr" {
+  name                = "acrvaibhavrag2026" 
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# --- OUTPUTS ---
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
